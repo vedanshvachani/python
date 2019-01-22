c527f08140903ae26841d71781d589060c58b2c0
vedanshvachani <vedanshvachani16@gmail.com>
2019-01-22 03:09:02 -0500

    Update directory_Size.py

--- directory_Size.py
+++ directory_Size.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
+#This program prins the name, path and the size of the direcotry. 
 import os
-root_name = raw_input("enter the directories name with full path = ")
+dir_name = raw_input("enter the directories name with full path = ")
 
 def dirsName(root_name):
     for root, dirs, files in os.walk(root_name,topdown=True):
@@ -11,7 +12,7 @@ def dirsName(root_name):
 	    dirs_size = os.path.getsize(os.path.abspath(os.path.join(root,i)))
 	    print "Size of the directory = ", str(dirs_size)
 
-if(os.path.exists(root_name)):
-    dirsName(root_name)
+if(os.path.exists(dir_name)):
+    dirsName(dir_name)
 else:
     print("Invalid Path")
